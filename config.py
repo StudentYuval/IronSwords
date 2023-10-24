b0bf8d91d70ed653d13a90f5befb36ce903a34e8
StudentYuval <114148324+StudentYuval@users.noreply.github.com>
2023-10-24 09:49:17 +0000

    db details

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DB_HOST = 'host'
-DB_USER = 'user'
-DB_PASS = 'pass'
-DB_NAME = 'name'
+DB_HOST = 'ironswords-server.mysql.database.azure.com'
+DB_USER = 'nnovelnmqd'
+DB_PASS = 'KilingBots1'
+DB_NAME = 'ironswords-database'
